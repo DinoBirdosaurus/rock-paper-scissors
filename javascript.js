08edf7218b41243961764341d0630ee77a911482
dinobirdosaurus <mynameispoch@gmail.com>
2022-12-08 14:36:12 +0800

    add 2 files index.html & javascript.js forgot to commit early first commit already contains codes

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const gameChoices = ["Rock", "Paper", "Scissors"];
+const getComputerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+    console.log(getComputerChoice);
+
+// STEP 1
+// label variable 'rock' 
+// label variable 'paper'
+// label variable 'scissors'
+// get computer to randomly return 1 of 3 variables(rock, paper, scissors)
+// STEP 2
+// user inputs choice
+// computer randomly makes a choice
+// 
