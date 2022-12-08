87be63abaff6d3b4a38b3d0938d29ad188fc73f3
dinobirdosaurus <mynameispoch@gmail.com>
2022-12-08 23:26:47 +0800

    edit .js add if else conditions

--- javascript.js
+++ javascript.js
@@ -2,13 +2,12 @@
 // create array to store into gameChoices
 // get computer to randomly return 1 of 3 variables(rock, paper, scissors)
 
-const gameChoices = ["Rock", "Paper", "Scissors"];
+const gameChoices = ["rock", "paper", "scissors"];
 const getComputerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
     console.log(getComputerChoice);
-  // added a second randomizer posing as Player to simulate win-lose-tie scenarios without manual user input  
+   //added a second randomizer posing as Player to simulate win-lose-tie scenarios without manual user input  
 const getPlayerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
     console.log(getPlayerChoice);
-
 
 // STEP 2
 // create function that starts 1 round of R,P,S
@@ -24,11 +23,35 @@ const getPlayerChoice = gameChoices[Math.floor(Math.random() * gameChoices.lengt
 
 function roundOne(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-            return ('Tie!');
-    } else {
-            return ('Lose!');
+            return 'Tie!';
+    } 
+    if (playerSelection === "paper")
+        if (computerSelection === "rock") {
+                return "You Win! Paper beats Rock!";
+            } else {
+                if (computerSelection === "scissors") {
+                    return "You Lose! Scissors beats Paper!"
+                }
+            }
+    
+
+    if (playerSelection === "rock") 
+        if (computerSelection === "paper") {
+                return "You Lose! Paper beats Rock!";
+            }  else {
+             if (computerSelection === "scissors") {
+                return "You Win! Rock beats Scissors!";
+             }
+        }
+    if (playerSelection === "scissors")
+        if (computerSelection === "paper") {
+            return "You Win! Scissors beats Paper!";
+        } else {
+            if (computerSelection === "rock") {
+                return "You Lose! Rock beats Scissors!";
+            }
+        }
     }
-}
 const playerSelection = getPlayerChoice;
 const computerSelection = getComputerChoice;
     console.log(roundOne(playerSelection, computerSelection));
