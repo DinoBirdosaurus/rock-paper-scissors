839de2c2141df49bfce204ae6b07d3f64ed37b7f
dinobirdosaurus <mynameispoch@gmail.com>
2022-12-08 17:15:55 +0800

    edit .js file add new code if/else statement for game

--- javascript.js
+++ javascript.js
@@ -1,13 +1,34 @@
+// STEP 1
+// create array to store into gameChoices
+// get computer to randomly return 1 of 3 variables(rock, paper, scissors)
+
 const gameChoices = ["Rock", "Paper", "Scissors"];
 const getComputerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
     console.log(getComputerChoice);
+  // added a second randomizer posing as Player to simulate win-lose-tie scenarios without manual user input  
+const getPlayerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+    console.log(getPlayerChoice);
 
-// STEP 1
-// label variable 'rock' 
-// label variable 'paper'
-// label variable 'scissors'
-// get computer to randomly return 1 of 3 variables(rock, paper, scissors)
+
 // STEP 2
-// user inputs choice
-// computer randomly makes a choice
-// 
+// create function that starts 1 round of R,P,S
+// define hierarchy to R,P,S choices
+    //Rock is greater than Scissors
+    // Paper is greater than Rock
+    // Scissors is greater than Paper
+    //If param 1 is equal to param 2, its a tie.
+// function should take 2 parameters and determine result as winner, loser or a tie
+// If winner, print out "You Win!, Param1 beats Param2"
+// If loser, print out " You Lose!, Param1 beats Param2"
+// If tie, print out "Its a Tie!, Try Again"
+
+function roundOne(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+            return ('Tie!');
+    } else {
+            return ('Lose!');
+    }
+}
+const playerSelection = getPlayerChoice;
+const computerSelection = getComputerChoice;
+    console.log(roundOne(playerSelection, computerSelection));
