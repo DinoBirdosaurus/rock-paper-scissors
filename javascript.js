f8d37935fea74f3c864939ce3cf1c80c45d6b1f4
dinobirdosaurus <mynameispoch@gmail.com>
2022-12-10 20:22:05 +0800

    edit .js file complete rock, paper, scissors game Notes: after nearly 3 days, finally got it. underwent 1 major overhaul. The assignment was hard but I learned a lot.

--- javascript.js
+++ javascript.js
@@ -1,70 +1,87 @@
-// STEP 1
-// create array to store into gameChoices
-// get computer to randomly return 1 of 3 variables(rock, paper, scissors)
-
-const gameChoices = ["rock", "paper", "scissors"];
-const getComputerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
-    console.log(getComputerChoice);
-   //added a second randomizer posing as Player to simulate win-lose-tie scenarios without manual user input  
-//const getPlayerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
-   // console.log(getPlayerChoice);
-
-// STEP 2
-// create function that starts 1 round of R,P,S
-// define hierarchy to R,P,S choices
-    //Rock is greater than Scissors
-    // Paper is greater than Rock
-    // Scissors is greater than Paper
-    //If param 1 is equal to param 2, its a tie.
-// function should take 2 parameters and determine result as winner, loser or a tie
-// If winner, print out "You Win!, Param1 beats Param2"
-// If loser, print out " You Lose!, Param1 beats Param2"
-// If tie, print out "Its a Tie!, Try Again"
-
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection == computerSelection) {
-                return 'Tie!';
-        } 
-        if (playerSelection === "paper")
-            if (computerSelection === "rock") {
-                    return "You Win! Paper beats Rock!";
-                } else {
-                   if (computerSelection === "scissors") {
-                        return "You Lose! Scissors beats Paper!"
-                    }
-              }
-        if (playerSelection === "rock") 
-            if (computerSelection === "paper") {
-                    return "You Lose! Paper beats Rock!";
-                } else {
-                    if (computerSelection === "scissors") {
-                        return "You Win! Rock beats Scissors!";
-                    }
-            }
-         if (playerSelection === "scissors")
-            if (computerSelection === "paper") {
-                    return "You Win! Scissors beats Paper!";
-                } else {
-                    if (computerSelection === "rock") {
-                        return "You Lose! Rock beats Scissors!";
-                    }
-                }
-            }
-//const playerSelection = getPlayerChoice;
-const computerSelection = getComputerChoice;
-   // console.log(playRound(playerSelection, computerSelection));
-
-//STEP 3: 5 round game, keep scores
-// make function called game()
-// call playRound function inside game() function
-// create 5 round loop game while keeping score
-// how to tell computer to keep score??
-// computerselection won't randomize each round
-// at end of round declare winner or loser or tie
-
-for (let i = 0; i <= 5; i++) {
-    let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-    const computerSelection = getComputerChoice;
-    let roundResult = playRound(playerSelection, computerSelection);
-        console.log(roundResult);
+// computer choice randomizer
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
     }
+
+};
+//score keeping
+let userScore = 0;
+let compScore = 0;
+
+//player input
+const getPlayerChoice = userInput => {
+    userInput = prompt("Rock, Paper or Scissor?").toLowerCase();
+    if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
+        return userInput;
+    } else {
+        console.log("Error, please type: rock, paper or scissors.");
+    }
+};
+
+//game logic
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+        userScore++;
+        compScore++;
+        return "Draw!"
+    } 
+    if (playerSelection === "rock")
+        if (computerSelection === "paper") {
+            compScore++;
+            return "You Lose! Paper covers Rock.";
+        } else {
+            if (computerSelection === "scissors") {
+                userScore++;
+                return "You Win! Rock crushes Scissors";
+            }
+        }
+    if (playerSelection === "paper")
+        if (computerSelection === "rock") {
+            userScore++;
+            return "You Win! Paper covers Rock.";
+        } else {
+            if (computerSelection === "scissors") {
+                compScore++;
+                return "You Lose! Scissors cut Paper.";
+            }
+        }
+    if (playerSelection === "scissors")
+        if (computerSelection === "rock") {
+            compScore++;
+            return "You Lose! Rock crushes Scissors.";
+        } else {
+            if (computerSelection === "paper") {
+                userScore++;
+                return "You Win! Scissors cut Paper.";
+            }
+        }
+    
+};
+
+//actual game function
+const game = () => {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log("You chose: " + playerSelection);
+        console.log("The computer chose: " + computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("Your Score: " + userScore);
+        console.log("Computer Score: " + compScore);
+        console.log("=========================")
+    }
+};
+
+game();
+
+
+
+
+
