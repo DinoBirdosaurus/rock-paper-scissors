96fcd61ee1792423429a6d5571a42bcd56ff5d86
dinobirdosaurus <mynameispoch@gmail.com>
2023-01-11 01:33:52 +0800

    finished v1.1 add alert message I'm giving up for now but I will come back to make it nicer

--- javascript.js
+++ javascript.js
@@ -1,35 +1,34 @@
+// I promise to make this game better in the future when I have acquired more experience. For now this is all I've got.
 let buttons = document.querySelectorAll(".button");
 const evalText = document.querySelector("#eval-text");
 const eval = document.querySelector("#eval");
+let gameLog = document.createElement('ul');
+gameLog.classList.add('round-result');
+const resetGame = document.createElement('div');
+resetGame.textContent = 'Try Again?';
+resetGame.classList.add('button', 'refresh');
 
 //score keeping
 let userScore = 0;
 let compScore = 0;
 
-//old player input before ui addition
-/*
-const getPlayerChoice = userInput => {
-    userInput = prompt("bolter, storm-shield or Scissor?").toLowerCase();
-    if (userInput === "bolter" || userInput === "storm-shield" || userInput === "chainsword") {
-        return userInput;
-    } else {
-        console.log("Error, please type: bolter, storm-shield or chainsword.");
-    }
-};
-*/
 
+// activates divs to turn into clickable buttons for player choice
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         const img = button.querySelector("img");
         const computerSelection = getComputerChoice();
         playerSelection = img.alt.toLowerCase(); // this translates image into playerSelection via alt string
         playRound(playerSelection, computerSelection);
+        if (userScore === 5 || compScore === 5) {
+            refreshPage();
+        }
     //console.log()s for debugging
-        console.log(`You chose: ${capitalize(playerSelection)}`);
+     /*   console.log(`You chose: ${capitalize(playerSelection)}`);
         console.log(`The computer chose: ${capitalize(computerSelection)}`);
         console.log("Your Score: " + userScore);
         console.log("Computer Score: " + compScore);
-        console.log("=========================")
+        console.log("=========================")*/
     // win condition
         
     })
@@ -48,9 +47,7 @@ const getComputerChoice = () => {
 
 };
 
-const capitalize = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
+
 
 //game logic
 const playRound = (playerSelection, computerSelection) => {
@@ -72,6 +69,8 @@ const playRound = (playerSelection, computerSelection) => {
 
             if (userScore == 5) {
                 result = "Congrats! You pass the training module.";
+                declareWinner();
+               // refreshPage();
             }
         
             
@@ -85,19 +84,54 @@ const playRound = (playerSelection, computerSelection) => {
         
             if (compScore == 5) {
                 result = "You failed! Try Again!";
+                declareWinner();
+               // refreshPage();
             }    
               
-    
+// results go at the bottom
     document.getElementById("playerScore").innerHTML = userScore;
     document.getElementById("opponentScore").innerHTML = compScore;
     document.getElementById("results-text").innerHTML = result;
 };    
-    
+// declares winner
+const declareWinner = () => {
+    if (userScore > compScore) {
+        alert ("Congrats! You pass the training module");
+        //evalText.textContent = "Congrats! You pass the training module";
+       // resetGame.innerText = "Try Again?";
+    } else {
+        alert ("You failed! Try Again!");
+       // evalText.textContent = "You failed! Try Again!";
+      //  resetGame.innerText = "Try Again?";
+    }
+};
+// section for additional vanity functions
+const capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
+const refreshPage = () => {
+    window.location.reload(true);
+}
+// for reset game button when I figure it out
+resetGame.addEventListener('click', refreshPage);
+
+/* =============================================================================
+
+//old player input before ui addition
+
+const getPlayerChoice = userInput => {
+    userInput = prompt("bolter, storm-shield or Scissor?").toLowerCase();
+    if (userInput === "bolter" || userInput === "storm-shield" || userInput === "chainsword") {
+        return userInput;
+    } else {
+        console.log("Error, please type: bolter, storm-shield or chainsword.");
+    }
+};
 
 
 //old game function
-/*
+
 const game = () => {
     for (let i = 0; i < 5; i++) {
         const playerSelection = getPlayerChoice();
